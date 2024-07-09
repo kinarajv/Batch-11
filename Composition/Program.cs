5c67b8e44c3a099c2205bd5d0211984acefd2392
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-07-09 14:59:49 +0700

    Change ...

--- Composition/Program.cs
+++ Composition/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
 	static void Main() 
 	{
-		Engine engine = new Engine("V8", "5.0", "Mercedes");
+		Engine engine = new Engine("V8", "5.0", "Mercedess");
 		Tire tire = new Tire(18, "Rubber", "Michelin");
 		Car car = new Car(engine, tire);
 
