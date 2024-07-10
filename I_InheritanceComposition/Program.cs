a366285f21753fba59d401f66d39faa0af4a6337
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-07-10 13:20:37 +0700

    Inheritance Composition

---
+++ I_InheritanceComposition/Program.cs
@@ -0,0 +1,24 @@
+﻿//Inheritance Composition
+class Car {
+	public Engine engine;
+	public Car(Engine engine) {
+		this.engine = engine;
+	}
+}
+class Engine {
+	public int size;
+	public string brand;
+}
+class ElectricEngine : Engine {
+}
+class DieselEngine : Engine {} 
+class PistonEngine : Engine {}
+class Program {
+	static void Main() {
+		Engine engine = new Engine();
+		Car car = new Car(engine);
+		
+		ElectricEngine electricEngine = new ElectricEngine();
+		Car car2 = new Car(electricEngine);
+	}
+}
