69d9a5392bbaff8284821610702ffe0ca19ba43a
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-07-10 11:03:47 +0700

    Inheritance with custom constructor

---
+++ E_ErrorParentChildConstructor/Program.cs
@@ -0,0 +1,23 @@
+﻿//Error ParentChild Constructor
+class Component {
+	public string brand;
+	public string colour;
+	public int price;
+	public Component(string brand, string colour, int price)
+	{
+		this.brand = brand;
+		this.colour = colour;
+		this.price = price;
+	}
+}
+class Bell : Component {
+	public int size;
+	public string model;
+	public string material;
+	//below error because requirement from Parent
+	public Bell(int size, string model, string material) {
+		this.size = size;
+		this.model = model;
+		this.material = material;
+	}
+}
---
+++ F_SolutionParentChildConstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿//ParentChild Constructor
+class Component {
+	public string brand;
+	public Component(string brand)
+	{
+		this.brand = brand;
+	}
+}
+class Bell : Component {
+	public int size;
+	public Bell(string brand, int size) : base(brand) 
+	{
+		this.size = size;
+	}
+}
+class Program {
+	static void Main() {
+		Bell bell = new Bell("toyota", 12);
+	}
+}
