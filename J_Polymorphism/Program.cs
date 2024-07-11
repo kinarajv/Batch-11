15f99e572dfb68d0322736d5243328ee9b0069fb
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-07-11 11:09:57 +0700

    Overriding

---
+++ J_Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿//Overriding
+class Animal {
+	public string name;
+	public int age;
+	public void Eat() {
+		Console.WriteLine("Eat");
+	}
+	public virtual void MakeSound() {
+		Console.WriteLine("...");
+	}
+}
+class Cat : Animal {
+	public override void MakeSound() {
+		Console.WriteLine("Meow!");
+	}
+}
+class Ant : Animal {}
+class Program {
+	static void Main() {
+		Animal animal = new();
+		animal.MakeSound(); //...
+		Cat cat = new();
+		cat.MakeSound(); //Meow
+		Ant ant = new();
+		ant.MakeSound(); //...
+	}
+}
