c64ac47382166ec1c4908dfab6c9b4005f69b51a
Kinara <91768638+kinarajv@users.noreply.github.com>
2024-07-11 14:02:54 +0700

    Add Overloading

---
+++ L_Polymorphism_Overloading/Calculator.cs
@@ -0,0 +1,16 @@
+namespace L_Polymorphism_Overloading;
+
+public class Calculator
+{
+	public int Add(int a, int b) {
+		return a + b;
+	}
+	public float Add(float a, float b) 
+	{
+		return a + b;
+	}
+	public string Add(string a, string b) 
+	{
+		return a + b;
+	}
+}
---
+++ L_Polymorphism_Overloading/Program.cs
@@ -0,0 +1,21 @@
+﻿using L_Polymorphism_Overloading;
+
+class Program {
+	static void Main() {
+		Calculator calculator = new Calculator();
+		int a = 1;
+		int b = 2;
+		int result = calculator.Add(a, b);
+		Console.WriteLine(result);
+
+		float fa = 1;
+		float fb = 2;
+		float fresult = calculator.Add(a, b);
+		Console.WriteLine(fresult);
+
+		string sa = "1";
+		string sb = "2";
+		string sresult = calculator.Add(sa, sb);
+		Console.WriteLine(sresult);
+	}
+}
