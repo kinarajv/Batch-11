198ddac4ce19320c80ca3bdb51939aa3544f31b7
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-15 08:47:44 +0700

    Add Property

---
+++ A_Properties/Program.cs
@@ -0,0 +1,12 @@
+﻿class Car {
+	private int _price; //variable
+	public int GetPrice() {
+		return _price;
+	}
+	public void SetPrice(int value) {
+		_price = value;
+	}
+}
+class CarWProperty { //property
+	public int Price {get; private set;}
+}
