efa502a6f8a687a229c7b30889b91c0c29c0b840
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-16 11:14:54 +0700

    Add OperatorOverloading

---
+++ E_GenericConstraint3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+class Calculator
+{
+	public T Add<T>(T a, T b)  where T : IAdditionOperators<T, T, T> 
+    {
+		return a + b;
+	}
+}
+class Program {
+	static void Main() {
+		Calculator calc = new();
+		calc.Add<int>(3,4);
+		calc.Add<float>(3.0f, 2.0f);
+		calc.Add<decimal>(3.0M, 2.0M);
+		calc.Add<double>(3.0, 2.0);
+	}	
+}
---
+++ F_OperatorOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿//Operator Overloading
+class Program {
+	static void Main() {
+		Car car = new Car();
+		Car car2 = new Car();
+		Car result = car + car2;
+		Console.WriteLine(result.price);
+	}	
+}
+class Car {
+	public int price = 100;
+	public static Car operator+(Car a, Car b){
+		int result = a.price + b.price;
+		Car car = new Car();
+		car.price = result;
+		return car;
+	}
+	public static Car operator++(Car a) {
+		a.price++;
+		return a;
+	}
+}
