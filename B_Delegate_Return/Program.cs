a6717e2d57565d1085102a33fc5fb01a7f497827
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-17 08:40:09 +0700

    Add Delegate invocation List

---
+++ B_Delegate_Return/Program.cs
@@ -0,0 +1,24 @@
+﻿//Delegate Return
+public delegate int MyDelegate(int a, int b);
+class Program {
+	static void Main() {
+		Calculator calc = new();
+		MyDelegate del = calc.Add;
+		del += calc.Sub;
+		del += calc.Mul;
+		
+		int result = del.Invoke(5, 3);
+		Console.WriteLine(result);
+	}
+}
+class Calculator {
+	public int Add(int a, int b) {
+		return  a + b;
+	}
+	public int Sub(int a, int b) {
+		return a - b;
+	}
+	public int Mul(int a, int b) {
+		return a * b;
+	}
+}
---
+++ C_Delegate_InvocationList/Program.cs
@@ -0,0 +1,35 @@
+﻿//Delegate Invocation List
+public delegate int MyDelegate(int a, int b);
+class Program {
+	static void Main() {
+		Calculator calc = new();
+		MyDelegate del = calc.Add;
+		del += calc.Sub;
+		del += calc.Mul;
+		
+		int[] result = new int[3];
+		
+		Delegate[] invocationList = del.GetInvocationList();
+		
+		int count = 0;
+		foreach(MyDelegate method in invocationList) {
+			result[count] = method.Invoke(5,3);
+			count++;
+		}
+		foreach(var i in result) 
+        {
+            Console.WriteLine(i);
+        }
+	}
+}
+class Calculator {
+	public int Add(int a, int b) {
+		return  a + b;
+	}
+	public int Sub(int a, int b) {
+		return a - b;
+	}
+	public int Mul(int a, int b) {
+		return a * b;
+	}
+}
