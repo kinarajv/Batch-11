0352957279e0d77fb0d694cafb320d7cc3e9aaf7
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-17 08:21:53 +0700

    event keyword

---
+++ A_event_Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿public delegate void MyDelegate();
+class Publisher {
+	public event MyDelegate del;
+	public void Reset() {
+		del = null;
+	}
+}
+class Subscriber 
+{
+	public void GetNotification() {}
+}
+class Program {
+	static void Main() {
+		Publisher pub = new();
+		Subscriber sub = new();
+
+		// pub.del = sub.GetNotification; Cant use =
+		pub.del += sub.GetNotification;
+		pub.del -= sub.GetNotification;
+	}
+}
