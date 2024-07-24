4c7244ba9b6d6e379c7a72c7af9b52fad9c96f14
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-24 14:30:29 +0700

    Add String vs StringBuilder

---
+++ A_StringBuilderMutable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+class Program 
+{
+	static void Main() {
+		StringBuilder a = new("Hello");
+		int iteration = 100_000_000;
+		
+		for(int i = 0;i < iteration;i++) 
+		{
+			a.Append("World");
+			a.Append("!");
+			a.Replace("a", "i");
+		}
+	} 
+}
---
+++ A_StringImmutable/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program 
+{
+	static void Main() {
+		string a = "a";
+		int iteration = 100_000_000;
+		
+		for(int i = 0;i < iteration;i++) 
+		{
+			a += "b";
+			a += "!";
+			a.Replace("a", "c");
+			Thread.Sleep(2);
+		}
+	} 
+}
