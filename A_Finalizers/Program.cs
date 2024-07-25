9e00a1c901531b430f850c3ffa89a052ac653e9d
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-25 10:28:57 +0700

    Add Finalizers

---
+++ A_Finalizers/Program.cs
@@ -0,0 +1,23 @@
+﻿class MyDestructor 
+{
+	public MyDestructor() 
+	{
+		Console.WriteLine($"MyDesctructor {GC.GetGeneration(this)} created");
+	}
+	~MyDestructor()
+	{
+		Console.WriteLine($"MyDestructor {GC.GetGeneration(this)}  destructed");
+	}
+}
+class Program 
+{
+	static void Main() 
+	{
+		InstanceCreator();
+		GC.Collect();
+	}
+	static void InstanceCreator() 
+	{
+		MyDestructor myDestructor = new();
+	}
+}
