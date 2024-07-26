ab7329248d28d0766b49327229d5ee81186c8c9c
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-26 09:19:17 +0700

    Extension Method

--- J_ExtensionMethod/Program.cs
+++ J_ExtensionMethod/Program.cs
@@ -3,6 +3,17 @@
 	static void Main() 
 	{
 		string a = "Hello";
-		a.Cetak();
+		a.Dump();
+		int x = 3;
+		x.Dump();
+		float[] myFloats = { 1.0f, 2.0f };
+		myFloats.Dump();
+	}
+}
+public static class MyExtensions 
+{
+	public static void Dump(this object value) 
+	{
+		Console.WriteLine(value);
 	}
 }
