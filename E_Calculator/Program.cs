9b22b31dc07eb27260cefdc590f87ceb5ccff6ff
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-26 13:56:30 +0700

    Add Debugger

---
+++ E_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+class Program 
+{
+	static void Main() 
+	{
+		Console.WriteLine("Program starting");
+		Calculator calculator = new();
+		int result = calculator.Add(3, 5);
+		Console.WriteLine(result);
+	}
+}
+class Calculator 
+{
+	public int Add(int a, int b) => a * b;
+}
