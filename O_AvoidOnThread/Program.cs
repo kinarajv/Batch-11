2aa6d6a553e271af6067bc80aeccca641651b789
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-29 11:49:19 +0700

    Add Forbidden usages on Thread

---
+++ O_AvoidOnThread/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program {
+    static void Main() {
+        Thread t1 = new Thread(() => { });
+        t1.Priority = ThreadPriority.Highest; //Forbidden
+        t1.Start();
+        t1.Abort(); //Forbidden
+    }
+}
