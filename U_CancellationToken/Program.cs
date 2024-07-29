01d0837d35562cd21c6dc24e4903ceee36c76723
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-29 14:53:25 +0700

    Add CancellationToken

---
+++ U_CancellationToken/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program berjalan, memantau gempa...");
+		CancellationTokenSource alarmCts = new CancellationTokenSource();
+        Task t2 = Task.Run(() => MonitorGempa(alarmCts.Token));
+		Task t1 = Task.Run(() =>
+        {
+           	Thread.Sleep(5000);
+            alarmCts.Cancel(); 
+        });
+		Task.WaitAny(t1,t2);
+        Console.ReadLine(); 
+    }
+
+    static void MonitorGempa(CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            Console.WriteLine("Monitoring gempa");
+            Thread.Sleep(1000); 
+        }
+        Console.WriteLine("Gempa nih...");
+    }
+}
