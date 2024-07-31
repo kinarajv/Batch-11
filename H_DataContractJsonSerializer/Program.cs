436115c0f59875a32c69ad0eadbe7ab7f78088f5
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-07-31 10:07:25 +0700

    Rearrange

--- H_DataContractJsonSerializer/Program.cs
+++ H_DataContractJsonSerializer/Program.cs
@@ -18,34 +18,34 @@ public class Human
 	}
 	public string GetName() => _name;
 	public int GetAge() => _age;
-	public enum HumanType
+}
+public enum HumanType
+{
+	Manusia,
+	Dinasti
+}
+class Program
+{
+	static void Main()
 	{
-		Manusia,
-		Dinasti
-	}
-	class Program
-	{
-		static void Main()
+		Human human = new Human("Prabowi", 72, HumanType.Dinasti);
+
+		DataContractJsonSerializer dataContract = new(typeof(Human
+		));
+		//Write
+		using (FileStream fs = new("./myPresident.json", FileMode.Create))
 		{
-			Human human = new Human("Prabowi", 72, HumanType.Dinasti);
-
-			DataContractJsonSerializer dataContract = new(typeof(Human
-			));
-			//Write
-			using (FileStream fs = new("./myPresident.json", FileMode.Create))
-			{
-				dataContract.WriteObject(fs, human);
-			}
-
-			//Read
-			Human human2;
-			using (FileStream fs = new("./myPresident.json", FileMode.Open))
-			{
-				human2 = (Human)dataContract.ReadObject(fs);
-			}
-			Console.WriteLine(human.GetName());
-			Console.WriteLine(human.GetAge());
-
+			dataContract.WriteObject(fs, human);
 		}
+
+		//Read
+		Human human2;
+		using (FileStream fs = new("./myPresident.json", FileMode.Open))
+		{
+			human2 = (Human)dataContract.ReadObject(fs);
+		}
+		Console.WriteLine(human.GetName());
+		Console.WriteLine(human.GetAge());
+
 	}
 }
