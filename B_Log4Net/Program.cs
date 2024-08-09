c0be9d94b1e919c4b1d87d83708d8dc6f8f5dfa5
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-08-09 08:58:33 +0700

    Add Logger

---
+++ B_Log4Net/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using log4net;
+using log4net.Config;
+
+class Program 
+{
+	static void Main() 
+	{
+		ILog logger = LogManager.GetLogger(typeof(Robot));
+		XmlConfigurator.Configure(new FileInfo("log4net.config"));
+		Robot robot = new Robot(logger);
+		while(true) 
+		{	
+			robot.MoveRight();
+			robot.MoveLeft();
+			robot.TurnOff();
+			robot.MoveRight();
+			robot.MoveLeft();
+			robot.TurnOff();
+		}
+	}
+}
+class Robot
+{
+	private ILog _log;
+	public Robot(ILog log) 
+	{
+		_log = log;
+	}
+	public void MoveLeft()
+	{
+		_log.Warn("Move Left executed");
+		Console.WriteLine("Move...");
+		Console.WriteLine("Move Left Finished");
+	}
+
+	public void MoveRight()
+	{
+		_log.Info("Move Right executed");
+		Console.WriteLine("Move...");
+		Console.WriteLine("Move Right Finished");
+	}
+
+	public void TurnOff()
+	{
+		_log.Error("TurnOff executed");
+		Console.WriteLine("TurnOff...");
+		Console.WriteLine("TurnOff Finished");
+	}
+}
---
+++ C_NLog/Program.cs
@@ -0,0 +1,54 @@
+﻿using NLog;
+
+class Program 
+{
+	static void Main() 
+	{
+		ILogger logger = LogManager.GetCurrentClassLogger();
+		Robot robot = new Robot(logger);
+		while(true) 
+		{	
+			robot.MoveRight();
+			robot.MoveLeft();
+			robot.TurnOff();
+			robot.MoveRight();
+			robot.MoveLeft();
+			robot.TurnOff();
+		}
+	}
+}
+class Robot
+{
+	private ILogger _log;
+	public Robot(ILogger log) 
+	{
+		_log = log;
+	}
+	public void MoveLeft()
+	{
+		string email = Console.ReadLine();
+		string password = Console.ReadLine();
+		_log.Info("Move Left executed");
+		if (email == "joko") {
+			
+			return;
+		}
+		
+		Console.WriteLine("Move...");
+		Console.WriteLine("Move Left Finished");
+	}
+
+	public void MoveRight()
+	{
+		_log.Info("Move Right executed");
+		Console.WriteLine("Move...");
+		Console.WriteLine("Move Right Finished");
+	}
+
+	public void TurnOff()
+	{
+		_log.Error("TurnOff executed");
+		Console.WriteLine("TurnOff...");
+		Console.WriteLine("TurnOff Finished");
+	}
+}
