ef46c3ca5bff3572dd4f4919d8f489581270ab7f
Kinara Jongga Varga <kinara.varga@formulatrix.com>
2024-08-12 10:07:07 +0700

    Add Unit Test

---
+++ MyCalculator.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+namespace MyCalculator.Test;
+
+public class MyCalculatorTest 
+{
+	private Calculator _calculator;
+	[SetUp]
+	public void SetUp() 
+	{
+		_calculator = new Calculator();
+	}
+	[Test]
+	public void Add_ReturnCorrectResult() 
+	{
+		//Arrange
+		int a = 3;
+		int b = 2;
+		int expected = 5;
+		//Act
+		int result = _calculator.Add(a, b);
+		//Assert
+		Assert.That(result, Is.EqualTo(expected));
+	}
+	[TestCase(3,4,7)]
+	[TestCase(5,8,13)]
+	[TestCase(13,-4,9)]
+	[TestCase(1000,1,1001)]
+	public void Add_ReturnCorrectResult(int a, int b, int expected) 
+	{
+		//Arrange
+		//Act
+		int result = _calculator.Add(a, b);
+		//Assert
+		Assert.That(result, Is.EqualTo(expected));
+	}
+}
---
+++ MyCalculator/Calculator.cs
@@ -0,0 +1,9 @@
+namespace MyCalculator;
+
+public class Calculator
+{
+	public int Add(int a, int b) 
+	{
+		return a + b;
+	}
+}
